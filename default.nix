0557b57432d07f77e7c6dcb0c51891d30c065a3f
Elliot Cameron <eacameron@gmail.com>
2019-08-19 00:56:20 -0400

    Bump gargoyle

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@
     gargoylePkg = import (pkgs.fetchFromGitHub {
       owner = "obsidiansystems";
       repo = "gargoyle";
-      rev = "e6fb0c932a615ec525ac2dab3d16acbf3d77da61";
+      rev = "1ddbfd14ecafa5225779967843d31f2a5f964deb";
       sha256 = "032jmfpyg740m5bynjmjcxxp2c773qad9b7494h9jg3vw4hp328g";
     }) self;
   in
