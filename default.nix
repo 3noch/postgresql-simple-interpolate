4ae2782923fd6672747b16b97d76a7782eb06f2d
Elliot Cameron <eacameron@gmail.com>
2019-08-19 00:56:37 -0400

    Filter source and apply hlint in default.nix

--- default.nix
+++ default.nix
@@ -1,5 +1,32 @@
 { pkgs ? import <nixpkgs> {}, haskellPackages ? pkgs.haskellPackages, ... }:
-{
+let
+  hsOnly = pkg: pkg.overrideAttrs ({ src, ... }: {
+    src = pkgs.lib.cleanSourceWith {
+      filter = (name: type: let baseName = baseNameOf (toString name); in
+        type == "directory" && !(builtins.elem baseName ["dist" "dist-newstyle"]) ||
+        builtins.elem baseName ["LICENSE"] ||
+        pkgs.lib.hasSuffix ".hs" baseName ||
+        pkgs.lib.hasSuffix ".cabal" baseName ||
+        pkgs.lib.hasSuffix ".nix" baseName ||
+        pkgs.lib.hasSuffix ".md" baseName
+      );
+      src = pkgs.lib.cleanSource (pkgs.lib.cleanSource src);
+    };
+  });
+
+  checkHLint = pkg: pkg.overrideAttrs ({ preConfigure ? "", src, ... }: {
+    preConfigure = ''
+      (
+        echo "Checking for lint"
+
+        set -x
+        '${pkgs.hlint}/bin/hlint' --version
+        '${pkgs.hlint}/bin/hlint' --hint '${builtins.path { path = ./.hlint.yaml; name = "hlint.yaml"; }}' '${src}' || exit 1
+      )
+      ${preConfigure}
+    '';
+  });
+in {
   haskellPackages = pkgs.haskellPackages.extend (self: super:
   let
     gargoylePkg = import (pkgs.fetchFromGitHub {
@@ -10,7 +37,7 @@
     }) self;
   in
   {
-    postgresql-simple-interpolate = self.callCabal2nix "postgresql-simple-interpolate" ./. {};
+    postgresql-simple-interpolate = checkHLint (hsOnly (self.callCabal2nix "postgresql-simple-interpolate" ./. {}));
     inherit (gargoylePkg) gargoyle gargoyle-postgresql gargoyle-postgresql-nix gargoyle-postgresql-connect;
   });
 }
